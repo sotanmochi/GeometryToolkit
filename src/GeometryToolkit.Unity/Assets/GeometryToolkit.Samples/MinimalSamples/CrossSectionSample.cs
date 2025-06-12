1591d68636b945e6dce83ed4e1121a95784b7485
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2025-06-12 18:56:02 +0900

    Update sample

--- src/GeometryToolkit.Unity/Assets/GeometryToolkit.Samples/MinimalSamples/CrossSectionSample.cs
+++ src/GeometryToolkit.Unity/Assets/GeometryToolkit.Samples/MinimalSamples/CrossSectionSample.cs
@@ -9,16 +9,16 @@ namespace GeometryToolkit.Samples
         {
             var plane = new Plane(Vector3.up, new Vector3(0, 0, 0));
 
-            var unitSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            var sphereMesh = unitSphere.GetComponent<MeshFilter>().mesh;
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var mesh = obj.GetComponent<MeshFilter>().mesh;
 
-            var meshData = MeshDataBuffer.Create(sphereMesh, unitSphere.transform);
+            var meshData = MeshDataBuffer.Create(mesh, obj.transform);
             var meshCutData = MeshCutter.Execute(new List<MeshDataBuffer>(){ meshData }, plane);
 
             var contours = ContourLoopExtractor.Execute(meshCutData);
             var crossSection = CrossSection.Generate(contours[0]);
 
-            Debug.Log($"Perimeter: {crossSection.Perimeter} [m], Area: {crossSection.SignedArea} [m²]");
+            Debug.Log($"Perimeter: {crossSection.Perimeter} [m], Area: {crossSection.SignedArea} [m²], Centroid: {crossSection.Centroid}");
         }
     }
 }
